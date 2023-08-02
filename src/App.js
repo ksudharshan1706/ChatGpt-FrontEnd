40aa62130ee647dc4952255257b4281749c75118
ksudharshan1706 <kamavaramsudharshan1706@gmail.com>
2023-08-02 12:24:21 +0530

    ChatGpt front end

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+// import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme.js";
+import Navbar from "./components/Navbar";
+import Register from "./pages/Register";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Summary from "./pages/Summary";
+import Paragraph from "./pages/Paragraph";
+import ChatBot from "./pages/ChatBot";
+import JsConverter from "./pages/JsConverter";
+import ScifiImage from "./pages/ScifiImage";
 
 function App() {
+  const theme = useMemo(() => createTheme(themeSettings(), []));
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/summary" element={<Summary />} />
+          <Route path="/paragraph" element={<Paragraph />} />
+          <Route path="/chatbot" element={<ChatBot />} />
+          <Route path="/js-converter" element={<JsConverter />} />
+          <Route path="/scifi-image" element={<ScifiImage />} />
+        </Routes>
+      </ThemeProvider>
+    </>
   );
 }
 
